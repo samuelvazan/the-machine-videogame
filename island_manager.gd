f59d949690d2bd77743aba96e9e07fb316409270
Samuel Važan <samuel.vazan@tutanota.com>
2026-09-12 18:27:25 +0200

    some architecture changes

---
+++ island_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+# Here, we'll create the entire machine graph. Then, we'll calculate the position of each Island.
+# I'll also implement a BSP graph (or something like that) around the local player.
+# It must support multiplayer.
